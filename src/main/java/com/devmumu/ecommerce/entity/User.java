b6c95d7924b77dee9f05bc0438d52f7bbe9f4958
mumu <123791887+whsmumu@users.noreply.github.com.>
2025-03-03 12:41:42 -0300

    feat: ORM entity User

--- src/main/java/com/devmumu/ecommerce/entity/User.java
+++ src/main/java/com/devmumu/ecommerce/entity/User.java
@@ -2,13 +2,28 @@ package com.devmumu.ecommerce.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String phone;
+
     private LocalDate birthDate;
+    
     private String password;
     
     public User(){
