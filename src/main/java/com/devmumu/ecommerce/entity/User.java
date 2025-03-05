bcf57596457cbd768cfe14092dbc0ab94c46c1d1
mumu <123791887+whsmumu@users.noreply.github.com.>
2025-03-04 23:16:23 -0300

    feat: adding unique in variable email

--- src/main/java/com/devmumu/ecommerce/entity/User.java
+++ src/main/java/com/devmumu/ecommerce/entity/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String phone;
